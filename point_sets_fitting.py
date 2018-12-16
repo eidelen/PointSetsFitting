2dfa2275447ae3ac516fb2a54e39303e303eb3b9
adrian.schneider@aot.swiss <adrian.schneider@aot.swiss>
2018-12-16 18:28:26 +0100

    Move point set to center

--- point_sets_fitting.py
+++ point_sets_fitting.py
@@ -9,3 +9,14 @@ def getPointSetCenter(aPointSet):
     assert isinstance(aPointSet, np.matrix)
 
     return aPointSet.sum(axis=1) * 1.0/aPointSet.shape[1]
+
+
+def movePointSetToCenter(aPointSet):
+    assert isinstance(aPointSet, np.matrix)
+
+    center = getPointSetCenter(aPointSet)
+    for i in xrange( aPointSet.shape[1] ):
+        aPointSet[:,i] = aPointSet[:,i] - center
+
+    return aPointSet
+
--- test_psf.py
+++ test_psf.py
@@ -13,5 +13,13 @@ class PsfTester(unittest.TestCase):
         self.assertTrue(np.array_equal(centerIs,centerComputed))
 
 
+    def test_move_to_center(self):
+
+        testInput = np.mat('0  0  0; 0  4  0 ; 8  0  0; 0  0  12').transpose()
+        movedAre = np.mat('-2  -1  -3; -2  3  -3 ; 6  -1  -3; -2  -1  9').transpose()
+        movedComputed = psf.movePointSetToCenter(testInput)
+        self.assertTrue(np.array_equal(movedAre,movedComputed))
+
+
 if __name__ == '__main__':
     unittest.main()
