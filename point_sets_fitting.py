e65c9983465a3d52abec93b868b9373ffc8d1205
adrian.schneider@aot.swiss <adrian.schneider@aot.swiss>
2018-12-16 23:04:51 +0100

    Implemented least square fitting of 3d point sets.

--- point_sets_fitting.py
+++ point_sets_fitting.py
@@ -2,21 +2,67 @@ import numpy as np
 
 
 def pointSetFitting( setA, setB ):
-    return
+    assert isinstance(setA, np.matrix)
+    assert isinstance(setB, np.matrix)
+
+    nbrPoints = setA.shape[1]
+
+
+    # move point sets to center
+    cSetA, transA = movePointSetToCenter(setA)
+    cSetB, transB = movePointSetToCenter(setB)
+
+
+    # compute rotation
+    h = np.asmatrix(np.zeros((3,3)))
+    for i in xrange(nbrPoints):
+        pA = cSetA[:,i]
+        pB = cSetB[:,i]
+
+        h = h + pA * pB.transpose()
+
+    u, s, vh = np.linalg.svd(h)
+
+    rotation = vh.transpose() * u.transpose()
+
+    # fix rotation if necessary -> reflections
+    if np.linalg.det(rotation) < 0 :
+        print("Reflection occured")
+        vhMod = np.asmatrix(np.zeros(vh.shape))
+        vhMod[:,3] = vh[:,3] * (-1)
+        rotation = vhMod.transpose() * u.transpose;
+
+
+    # compute translation
+    trans = transB - (rotation * transA)
+
+
+    # compose a 4x4 matrix of rotation and translation
+    rigidTransformation = np.asmatrix(np.eye(4,4))
+    rigidTransformation[0:3, 0:3] = rotation
+    rigidTransformation[0:3,3] = trans
+
+    #todo: compute transformation error
+    return rigidTransformation, 0
+
 
 
 def getPointSetCenter(aPointSet):
     assert isinstance(aPointSet, np.matrix)
 
-    return aPointSet.sum(axis=1) * 1.0/aPointSet.shape[1]
+    return np.asmatrix(aPointSet.sum(axis=1) * 1.0/aPointSet.shape[1])
+
 
 
 def movePointSetToCenter(aPointSet):
     assert isinstance(aPointSet, np.matrix)
 
+    nbrOfPoints = aPointSet.shape[1]
     center = getPointSetCenter(aPointSet)
-    for i in xrange( aPointSet.shape[1] ):
-        aPointSet[:,i] = aPointSet[:,i] - center
+    mvSet = np.asmatrix(np.zeros( aPointSet.shape ))
 
-    return aPointSet
+    for i in xrange(nbrOfPoints):
+        mvSet[:,i] = aPointSet[:,i] - center
+
+    return mvSet, center
 
--- test_psf.py
+++ test_psf.py
@@ -17,8 +17,42 @@ class PsfTester(unittest.TestCase):
 
         testInput = np.mat('0  0  0; 0  4  0 ; 8  0  0; 0  0  12').transpose()
         movedAre = np.mat('-2  -1  -3; -2  3  -3 ; 6  -1  -3; -2  -1  9').transpose()
-        movedComputed = psf.movePointSetToCenter(testInput)
+        centerIs = np.mat('2; 1; 3')
+
+        movedComputed, centerComputed = psf.movePointSetToCenter(testInput)
+
         self.assertTrue(np.array_equal(movedAre,movedComputed))
+        self.assertTrue(np.array_equal(centerIs, centerComputed))
+
+
+    def test_fitting_identity(self):
+
+        setA = np.mat('0 0 0; 1 0 0; 0 1 0; 0 0 2').transpose()
+        setB = np.mat('0 0 0; 1 0 0; 0 1 0; 0 0 2').transpose()
+
+        expectedError = 0;
+        expectedTransformation = np.asmatrix(np.eye(4,4))
+
+        transformation, err = psf.pointSetFitting(setA, setB)
+
+        np.testing.assert_array_almost_equal(expectedTransformation, transformation, decimal=5)
+        self.assertAlmostEqual(expectedError,err,delta = 0.0001)
+
+
+    def test_fitting_translation(self):
+
+        setA = np.mat('0 0 0 1; 1 0 0 1; 0 1 0 1; 0 0 2 1').transpose()
+        expectedTransformation = np.asmatrix(np.eye(4, 4))
+        expectedTransformation[0,3] = 10
+        expectedTransformation[1, 3] = 2
+        setB = expectedTransformation * setA
+
+        expectedError = 0;
+
+        transformation, err = psf.pointSetFitting(setA[0:3,:], setB[0:3,:])
+
+        np.testing.assert_array_almost_equal(expectedTransformation, transformation, decimal=5)
+        self.assertAlmostEqual(expectedError,err,delta = 0.0001)
 
 
 if __name__ == '__main__':
