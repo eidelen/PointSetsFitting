2a47d9880e93812a0e3d34c5226f92ef149173e2
adrian.schneider@aot.swiss <adrian.schneider@aot.swiss>
2021-05-11 10:21:36 +0200

    Finished parsing

--- cpp/app_psf_cmd.cpp
+++ cpp/app_psf_cmd.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <regex>
+#include <exception>
+#include <iomanip>
 
 #include "psf.h"
 
@@ -13,12 +15,38 @@ std::vector<std::string> split(const std::string& str, const std::string& regex_
     return list;
 }
 
+Eigen::MatrixXd parsePointSet(const std::string& input)
+{
+    auto setPosAsString = split(input, ";");
+    std::vector<std::tuple<double,double,double>> points;
+    for(const auto& posStr : setPosAsString)
+    {
+        auto compAsString = split(posStr, ",");
+        if( compAsString.size() != 3 )
+            throw std::invalid_argument("Three vector components separated by ',' required");
+
+        points.emplace_back( std::stod(compAsString[0]), std::stod(compAsString[1]), std::stod(compAsString[2]) );
+    }
+
+    return vectorOfPositions2EigenMatrix(points);
+}
+
 std::pair<Eigen::MatrixXd, Eigen::MatrixXd> processInput(const std::string& input)
 {
     auto pointSetsAsString = split(input, ":");
+    if(pointSetsAsString.size() != 2)
+        throw std::invalid_argument("Two point sets separated by ':' required");
 
+    return {parsePointSet(pointSetsAsString[0]), parsePointSet(pointSetsAsString[1])};
 }
 
+std::string matAsStr(const std::string& name, const Eigen::MatrixXd& mat)
+{
+    std::ostringstream oss;
+    Eigen::IOFormat matOutputFormat(Eigen::StreamPrecision, 0, ", ", ";\n", "[", "]", "[", "]");
+    oss << std::fixed << std::setprecision(5) << name << std::endl << mat.format(matOutputFormat) << std::endl;
+    return oss.str();
+}
 
 int main(int argc, char** argv)
 {
@@ -29,9 +57,15 @@ int main(int argc, char** argv)
 
     auto[setA, setB] = processInput(input);
 
+    Eigen::IOFormat matOutputFormat(Eigen::StreamPrecision, 0, ", ", ";\n", "[", "]", "[", "]");
 
+    std::cout << matAsStr("Point Set A:", setA.topRows(3)) << std::endl;
+    std::cout << matAsStr("Point Set B:", setB.topRows(3)) << std::endl;
 
-    std::cout << input << std::endl;
+    auto[ transformation, error ] = pointSetsFitting(setA, setB);
+
+    std::cout << matAsStr("Transformation A -> B:", transformation) << std::endl;
+    std::cout << std::fixed << std::setprecision(5) << "Fitting Error: " << error << std::endl;
 
     return 0;
 }
